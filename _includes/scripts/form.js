53a23837fa561d9f68e1bf641267eab65c5ba53e
chalatz <chralatz@gmail.com>
2024-12-23 17:21:04 +0200

    Added mailchimp form behavior

--- _includes/scripts/form.js
+++ _includes/scripts/form.js
@@ -17,42 +17,45 @@ function handle_form_behavior(){
 
     // let pristine = new Pristine(form, pristineConfig);
 
-    const the_select = document.getElementById('select-contact-method');
-    const the_email_heading = document.getElementById('email-heading');
-    const the_email = document.getElementById('email');
+    // const the_select = document.getElementById('select-contact-method');
+    const the_select = document.querySelector('[data-id="select-contact-method"]');
+    // const the_email_heading = document.getElementById('email-heading');
+    const the_email = document.getElementById('mce-EMAIL');
     const the_email_control = document.getElementById('email-control');
-    const the_phone = document.getElementById('phone');
+    const the_phone = document.getElementById('mce-PHONE');
     const the_phone_control = document.getElementById('phone-control');
-    const time_to_call = document.getElementById('time-to-call');
-    const time_to_call_control = document.getElementById('time_to_call-control');
+    // const time_to_call = document.getElementById('time-to-call');
+    // const time_to_call_control = document.getElementById('time_to_call-control');
+    const random_string = Math.random().toString(36).substring(7);
+    const random_email = 'random-' + random_string + '@secariolabs.com';
 
     the_select.addEventListener('change', (event) => {
         $this = event.target;
         const the_value = $this.value;
 
         if(the_value == 'phone') {
-            the_email.value = '';
-            the_email.required = false;
-            the_email_heading.classList.add('hidden');
-            the_email_control.classList.add('hidden');
+            the_email.value = random_email;
+            // the_email.required = false;
+            the_email_control.classList.add('hidden--important');
             
             the_phone.required = true;
-            time_to_call.required = true;
-            the_phone_control.classList.remove('hidden');
-            time_to_call_control.classList.remove('hidden');
+            // time_to_call.required = true;
+            the_phone_control.classList.remove('hidden--important');
+            // time_to_call_control.classList.remove('hidden--important');
         }
         
         if(the_value == 'email') {
+            the_email.value = '';
             the_phone.required = false;
-            time_to_call.required = false;
+            // time_to_call.required = false;
             the_phone.value = '';
-            time_to_call.selectedIndex = 0;
-            the_phone_control.classList.add('hidden');
-            time_to_call_control.classList.add('hidden');
+            // time_to_call.selectedIndex = 0;
+            the_phone_control.classList.add('hidden--important');
+            // time_to_call_control.classList.add('hidden--important');
 
-            the_email.required = true;
-            the_email_heading.classList.remove('hidden');
-            the_email_control.classList.remove('hidden');
+            // the_email.required = true;
+            // the_email_heading.classList.remove('hidden');
+            the_email_control.classList.remove('hidden--important');
         }
 
         // pristine = new Pristine(form, pristineConfig);
