2cfab5fe86b2a8ec2b46a93c258ec218850c5af5
chalatz <chralatz@gmail.com>
2024-12-31 19:55:26 +0200

    Added gup task that optimizes images

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,17 @@ var newer = require('gulp-newer');
 var imagemin = require('gulp-imagemin');
  
 var imgSrc = 'assets/images/**/*.{png,jpg,jpeg}';
-var tmp_opt = 'opt_images'
-var imgDest = 'dist/img';
+var imgDest = '_site/assets/images';
  
 // Minify any new images
 gulp.task('images', function() {
  
   // Add the newer pipe to pass through newer images only
-  return gulp.src(imgSrc)
+  return gulp.src(imgSrc, {encoding: false})
       .pipe(newer(imgDest))
       .pipe(imagemin())
       .pipe(gulp.dest(imgDest));
  
 });
  
-gulp.task('default', function() {
-  gulp.watch(imgSrc, ['images']);
-});
+gulp.task('go', gulp.series('images'));
